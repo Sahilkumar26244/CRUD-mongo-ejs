cfe606ab078bc073da764ab714e93bc564927321
Sahil Kumar <kumarsahil8981@gmail.com>
2022-11-05 10:06:13 +0530

    final touch

--- server.js
+++ server.js
@@ -32,4 +32,6 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(PORT, ()=> { 
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
